1a86ef894210d3f422c0142add89a4b047e0e100
Abhishek Punhani <punhani.manavabhi@gmail.com>
2024-11-24 20:09:15 +0530

    Added footer

---
+++ components/footer/copyright.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import { IoLocationSharp } from "react-icons/io5";
+import styles from "./styles.module.scss";
+interface props {
+  country: string;
+}
+
+export default function Copyright({ country }: props) {
+  return (
+    <div className={styles.footer__copyright}>
+      <section>©2022 SHOPPAY All Rights Resereved.</section>
+      <section>
+        <ul>
+          {data.map((link) => (
+            <li>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+          <li>
+            <a>
+              <IoLocationSharp /> {country}
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+const data = [
+  {
+    name: "Privacy Center",
+    link: "",
+  },
+  {
+    name: "Privacy & Cookie Policy",
+    link: "",
+  },
+  {
+    name: "Manage Cookies",
+    link: "",
+  },
+  {
+    name: "Terms & Conditions",
+    link: "",
+  },
+  {
+    name: "Copyright Notice",
+    link: "",
+  },
+];
---
+++ components/footer/index.tsx
@@ -0,0 +1,24 @@
+import Copyright from "./copyright";
+import Links from "./links";
+import NewsLetter from "./newsletter";
+import Payment from "./payment";
+import Socials from "./socials";
+import styles from "./styles.module.scss";
+
+interface FooterProps {
+  country: string;
+}
+
+export default function Footer({ country }: FooterProps) {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <Links />
+        <Socials />
+        <NewsLetter />
+        <Payment />
+        <Copyright country={country} />
+      </div>
+    </footer>
+  );
+}
